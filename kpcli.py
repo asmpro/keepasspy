178bd747418fcfbc6a146d868e844c19af8467a2
Uros Juvan <asmpro@gmail.com>
2014-05-31 02:03:49 +0200

    Added nicer tabular output and optional filtering by title or username and hiding/showing passwords.

--- kpcli.py
+++ kpcli.py
@@ -9,6 +9,7 @@ import os
 import sys
 import argparse
 import getpass
+import re
 
 # This is temporary development hack.
 # Later on libkeepass (including all it's requirements should be installed
@@ -26,6 +27,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--version', action='version', version='%(prog)s V{}'.format(VERSION))
 parser.add_argument('-f', '--file', dest='file', nargs='?', default='test.kdbx', help='KDB4 file (.kdbx)')
 parser.add_argument('-k', '--keyfile', dest='keyfile', nargs='?', help='Additional keyfile to use as master key')
+parser.add_argument('-p', '--password', dest='password', action='store_const', const=True, default=False, help='Shall we show password (if this argument is not given, asterisks are shown instead of password)')
+parser.add_argument('-t', '--title', dest='title', nargs='?', help='Optional regex value to filter only required titles')
+parser.add_argument('-u', '--username', dest='username', nargs='?', help='Optional regex value to filter only required usernames')
+# Add ability to filter by URL -l and notes -n as well
 args = parser.parse_args()
 
 if DEBUG:
@@ -35,11 +40,56 @@ if DEBUG:
 	if args.keyfile != None: print "Keyfile={}".format(args.keyfile)
 
 masterPassword = getpass.getpass("Master password: ")
-if DEBUG:
+if DEBUG >= 2:
 	print "Master password={}".format(masterPassword)
+
+showPasswords = args.password
+
+titleRe = None
+if args.title != None:
+	titleRe = re.compile(args.title, re.I)
+
+usernameRe = None
+if args.username != None:
+	usernameRe = re.compile(args.username, re.I)
 
 try :
 	with kp.open(args.file, password=masterPassword, keyfile=args.keyfile) as kdb:
-		print kdb.pretty_print()
+		if isinstance(kdb, kp.kdb3.KDB3Reader):
+			raise Exception("KDB3 (.kdb, KeePass 1.x) database format not supported!")
+		elif isinstance(kdb, kp.kdb4.KDB4Reader):
+			pass
+		else:
+			raise Exception("Unknown/unsupported database format implementation in libkeepass!")
+
+		#print kdb.pretty_print()
+		print "Title\tUsername\tPassword\tURL\tNotes"
+        for elem in kdb.obj_root.iterfind('.//Group/Entry'):
+        	title = ""
+        	username = ""
+        	password = ""
+        	url = ""
+        	notes = ""
+        	for sel in elem.iterfind('./String'):
+        		key = sel.find('./Key')
+        		val = sel.find('./Value')
+        		if key is None or val is None: continue
+
+        		if "Title" in key.text: title = val.text
+        		elif "UserName" in key.text: username = val.text
+        		elif "Password" in key.text:
+        			password = val.text
+        			if not showPasswords: password = "".join(map(lambda x: "*", password))
+        		elif "URL" in key.text: url = val.text
+        		elif "Notes" in key.text: notes = val.text
+
+        	# Check if filter allows showing data
+        	if titleRe != None:
+        		if titleRe.search(title) == None: continue
+        	if usernameRe != None:
+        		if usernameRe.search(username) == None: continue
+
+        	# Print out retrieved data
+        	print "\"{}\"\t\"{}\"\t\"{}\"\t\"{}\"\t\"{}\"".format(title, username, password, url, notes)
 except Exception as e:
 	print "ERROR: {}".format(e)
