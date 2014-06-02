558c3a5dc65ca9937cdfa53d4467be19db5966f8
urosj <asmpro@gmail.com>
2014-06-02 12:56:39 +0200

    Fixed some indenting, from previous indent change from tabs to spaces. Added support for pushing first matched entry's password to clipboard for max 12 seconds. After that it is automatically cleared. Maybe we will make time configurable with some parameter or extension of copy parameter.

--- kpcli.py
+++ kpcli.py
@@ -10,6 +10,8 @@ import sys
 import argparse
 import getpass
 import re
+import subprocess
+import time
 
 # This is temporary development hack.
 # Later on libkeepass (including all it's requirements should be installed
@@ -22,6 +24,46 @@ import libkeepass as kp
 VERSION="1.01"
 DEBUG=1
 
+# How long to keep password in the clipboard, before emptying it out
+
+# Function to copy given text to clipboard and wait (timeout seconds, before emptying cliboard out)
+def copyToClipboard(text, timeout=12):
+    if sys.platform == 'linux2':
+        prog = 'xclip'
+    elif sys.platform in ('win32', 'cygwin'):
+        prog = 'clip'
+    elif sys.platform == 'darwin':
+        prog = 'pbcopy'
+    else:
+        print "Cannot copy to cliboard: Unknown platform: {}".format(sys.platform)
+        return
+
+    # Try to pipe text to prog, else print error
+    try:
+        pipe = subprocess.Popen([prog], stdin=subprocess.PIPE)
+        pipe.communicate(text)
+    except Exception as e:
+        print "Unable to copy text to clipboard: {}".format(e)
+        return
+
+    # Wait for timeout seconds, outputing timeout seconds before clearing clipboard
+    waitTime = 0
+    while waitTime < timeout:
+        print "\r{}s".format(waitTime + 1),
+        sys.stdout.flush()
+        waitTime += 1
+        time.sleep(1)
+    print ""
+    print "Clearing out clipboard..."
+
+    try:
+        pipe = subprocess.Popen([prog], stdin=subprocess.PIPE)
+        pipe.communicate("")
+    except Exception as e:
+        print "Unable to copy text to clipboard: {}".format(e)
+        return
+
+
 # Parse command line options
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--version', action='version', version='%(prog)s V{}'.format(VERSION))
@@ -30,6 +72,7 @@ parser.add_argument('-k', '--keyfile', dest='keyfile', nargs='?', help='Addition
 parser.add_argument('-p', '--password', dest='password', action='store_const', const=True, default=False, help='Shall we show password (if this argument is not given, asterisks are shown instead of password)')
 parser.add_argument('-t', '--title', dest='title', nargs='?', help='Optional regex value to filter only required titles')
 parser.add_argument('-u', '--username', dest='username', nargs='?', help='Optional regex value to filter only required usernames')
+parser.add_argument('-c', '--copy', dest='copy', nargs='?', help='Optional requirement to copy specified field (password) to clipboard (if this function is supported for your OS)')
 # Add ability to filter by URL -l and notes -n as well
 args = parser.parse_args()
 
@@ -38,6 +81,10 @@ if DEBUG:
     #pprint(args)
     print "File={}".format(args.file)
     if args.keyfile != None: print "Keyfile={}".format(args.keyfile)
+    if args.password != None: print "Show passwords in clear"
+    if args.title != None: print "Show only titles matching regexp '{}'".format(args.title)
+    if args.username != None: print "Show only usernames matching regexp '{}'".format(args.username)
+    if args.copy != None: print "Copy {} field to clipboard".format(args.copy)
 
 masterPassword = getpass.getpass("Master password: ")
 if DEBUG >= 2:
@@ -64,32 +111,38 @@ try :
 
         #print kdb.pretty_print()
         print "Title\tUsername\tPassword\tURL\tNotes"
-    for elem in kdb.obj_root.iterfind('.//Group/Entry'):
-        title = ""
-        username = ""
-        password = ""
-        url = ""
-        notes = ""
-        for sel in elem.iterfind('./String'):
-            key = sel.find('./Key')
-            val = sel.find('./Value')
-            if key is None or val is None: continue
+        isfirst = True
+        for elem in kdb.obj_root.iterfind('.//Group/Entry'):
+            title = ""
+            username = ""
+            password = ""
+            url = ""
+            notes = ""
+            for sel in elem.iterfind('./String'):
+                key = sel.find('./Key')
+                val = sel.find('./Value')
+                if key is None or val is None: continue
 
-            if "Title" in key.text: title = val.text
-            elif "UserName" in key.text: username = val.text
-            elif "Password" in key.text:
-                password = val.text
-                if not showPasswords: password = "".join(map(lambda x: "*", password))
-            elif "URL" in key.text: url = val.text
-            elif "Notes" in key.text: notes = val.text
+                if "Title" in key.text: title = val.text
+                elif "UserName" in key.text: username = val.text
+                elif "Password" in key.text:
+                    origPassword = password = val.text
+                    if not showPasswords: password = "".join(map(lambda x: "*", password))
+                elif "URL" in key.text: url = val.text
+                elif "Notes" in key.text: notes = val.text
 
-        # Check if filter allows showing data
-        if titleRe != None:
-            if titleRe.search(title) == None: continue
-        if usernameRe != None:
-            if usernameRe.search(username) == None: continue
+            # Check if filter allows showing data
+            if titleRe != None:
+                if titleRe.search(title) == None: continue
+            if usernameRe != None:
+                if usernameRe.search(username) == None: continue
 
-        # Print out retrieved data
-        print "\"{}\"\t\"{}\"\t\"{}\"\t\"{}\"\t\"{}\"".format(title, username, password, url, notes)
+            # Print out retrieved data
+            print "\"{}\"\t\"{}\"\t\"{}\"\t\"{}\"\t\"{}\"".format(title, username, password, url, notes)
+
+            if isfirst and args.copy != None:
+                if args.copy == "password":
+                    copyToClipboard(origPassword)
+            isfirst = False
 except Exception as e:
     print "ERROR: {}".format(e)
